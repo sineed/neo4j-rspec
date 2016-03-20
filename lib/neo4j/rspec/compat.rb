7a04a63c332be5265f935b78cc7438c98f440568
Denis Tataurov <sineedus@mail.ru>
2016-03-20 19:20:24 +0300

    Naming changes

--- lib/neo4j/rspec/compat.rb
+++ lib/neo4j/rspec/compat.rb
@@ -4,13 +4,13 @@ module Neo4j
       class << self
         def current
           case Neo4j::VERSION
-          when /^6/ then Neo4j6.new
-          when /^7/ then Neo4j7.new
+          when /^6/ then Neo4jrb6.new
+          when /^7/ then Neo4jrb7.new
           end
         end
       end
 
-      class Neo4j6
+      class Neo4jrb6
         def property_nesting
           ActiveAttr::Typecasting
         end
@@ -20,7 +20,7 @@ module Neo4j
         end
       end
 
-      class Neo4j7
+      class Neo4jrb7
         def property_nesting
           Neo4j::Shared
         end
