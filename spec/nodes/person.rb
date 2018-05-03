704a5370b9a42c630683b82224f950ee031f298d
Brian Underwood <brian@brian-underwood.codes>
2018-05-03 09:08:44 -0400

    Rubocop: Style/SymbolArray

--- spec/nodes/person.rb
+++ spec/nodes/person.rb
@@ -2,7 +2,7 @@ class Person
   include Neo4j::ActiveNode
   has_many :in, :posts, origin: :author
   has_many :in, :comments, origin: :author
-  has_many :in, :written_things, type: false, model_class: [:Post, :Comment]
+  has_many :in, :written_things, type: false, model_class: %i[Post Comment]
 
   property :nickname
   property :reserved
